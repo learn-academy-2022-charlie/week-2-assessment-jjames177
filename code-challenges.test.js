c0d2d997e1cfee4175e1269cc4d0191f60382738
jjames177 <jjames17a@gmail.com>
2022-05-22 21:22:04 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,7 +12,7 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
+// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -24,46 +26,147 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 
 
-// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
+// --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+
+describe("divBy3", () => {
+    it("decides if the number is evenly divisible by three or not", () => {
+        const num1 = 15
+        // Expected outcome: "15 is divisible by three"
+        const num2 = 0 
+        // Expected output: "0 is divisible by three"
+        const num3 = -7
+        // Expected output: "-7 is not divisible by three"
+        expect(divBy3(num1)).toEqual("15 is divisible by three")
+        expect(divBy3(num2)).toEqual("0 is divisible by three")
+        expect(divBy3(num3)).toEqual("-7 is not divisible by three")
+    })
+})
+
+// I ran the test by inputting yarn jest into the terminal
+// I got red on the first test (good failure)
+// FAIL  ./code-challenges.test.js
+// divBy3
+// ✕ decides if the number is evenly divisible by three or not
+// ReferenceError: divBy3 is not defined
 
 
 // b) Create the function that makes the test pass.
 
+// -Psuedo Code-
+// Create a function named divBy3
+// divBy3 will take a number as an argument in the code
+// checking to see if the number is evenly divisible by 3
+// function will have a conditional if/else statement which will use num % 3 === 0
+// will return a string if true, which will state "is divisible by three"
+// will return a string if false, which will state " is not divisible by three"
+
+const divBy3 = (number) => {
+    if (number % 3 === 0) {
+        return `${number} is divisible by three`
+    } else {
+        return `${number} is not divisible by three`
+    }
+}
+
+// after running the test I received PASS
+// PASS  ./code-challenges.test.js
+// divBy3
+    // ✓ decides if the number is evenly divisible by three or not
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+describe("cap", () => {
+    it("returns an array with all the words capitalized", () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+        expect(cap(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(cap(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
 
+// I ran the test by inputting yarn jest into the terminal
+// I got red on the first test (good failure)
+// FAIL  ./code-challenges.test.js
+// cap
+    // ✕ returns an array with all the words capitalized (1 ms)
+// ReferenceError: cap is not defined
 
-// b) Create the function that makes the test pass.
+    // b) Create the function that makes the test pass.
 
+// -Psuedo Code-
+// Create a function named cap
+// The function should return an array with every single word being capitalized
+// Higher order function 
+// I will use .map method which will filter through the array
+// I will use .chartAt to isolate the index I want to capitalize of each string which will be 0 
+// I will also use the .toUpperCase method so the function will return the string in the array as uppercase
+// I will use a built in method .slice
 
+const cap = (array) => {
+    return array.map(value => {
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    })
+}
+
+// after running the test I received PASS
+// PASS  ./code-challenges.test.js
+//cap
+// ✓ returns an array with all the words capitalized (2 ms)
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+describe("fVowel", () => {
+    it("returns the index of the first vowel", () => {
+        const vowelTester1 = "learn"
+        // Expected output: 1
+        const vowelTester2 = "academy"
+        // Expected output: 0
+        const vowelTester3 = "challenges"
+        // Expected output: 2
+        expect(fVowel(vowelTester1)).toEqual([1])
+        expect(fVowel(vowelTester2)).toEqual([0])
+        expect(fVowel(vowelTester3)).toEqual([2])
+    })
+})
 
+// I ran the test by inputting yarn jest into the terminal
+// I got red on the first test (good failure)
+// FAIL  ./code-challenges.test.js
+//fVowel
+// ✕ returns the index of the first vowel (1 ms)
+//ReferenceError: fVowel is not defined
 
 // b) Create the function that makes the test pass.
+
+// -Psuedo Code-
+// I need to create a function named fVowel
+// fVowel will needs to iterate through all the letters in the given string
+// I will use a for loop which contains an if statement that will give me a vowel at every index
+// 
+
+
+
+
+const fVowel = (string) => {
+    for(let i = 0; i < string.length; i++){
+        if(string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u"){
+            return [i]
+        }
+    }
+}
+
+// after running the test I received PASS
+// PASS  ./code-challenges.test.js
+// fVowel
+// ✓ returns the index of the first vowel (2 ms)
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer: From what I can recall from our lectures, a parameter is basically a variable that acts as a placeholder. A parameter is placed inside the parentheses of the function expression. An argument is the additional info that the method requires and is passed in parentheses which is after the method name.
 
-  Researched answer:
+  Researched answer: After researching I found that the man difference between the two is that parameters are variables that are used as a major part of the function that is being defined. When a function is invoked the arguments are the key values that are passed.
 
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: Going back to this weeks lesson, I remember that .map takes predefined parameters and the required one is that the arrays value at the iteration that is current. The index of the array also at the current iteration is required.
 
-  Researched answer:
+  Researched answer: After doing some research I was wrong on my answer with the index of the array which is also at the current iteration is not required but optional. I also found out that I was missing one and that is that the array of the higher-order function (HOF) function.
 
 
 
 3. What is the difference between map and filter?
 
-  Your answer:
+  Your answer: From what I remember from the notes is that when map is used it will create a new array and this is done by changing every singe element individually in an array. With filter, a new array is created by taking out the elements that do not belong in your code.
 
-  Researched answer:
+  Researched answer: When using the map method, it allows what is in the array to be changed to whatever the person coding wants. Map will effect each item that is in the array and what will be returned is an array which will have the same length. With filter, depending on what criteria is given, it will make a key decision about every single item that is in the array and will return a given subset.
 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: Functions are sets of instructions that will return an output which is based on what input that was passed as an argument in its own parameter. A method is similar to a function in the way that it has sets of instructions which produce an outcome. The key difference is that methods are mainly used with objects.
 
-  Researched answer:
+  Researched answer: After researching, methods use bracket and dot notation and must be attached to the name of the associated object. A function can be called on by invoking its name. Functions can be on their own in the code but methods are the functions that are associated with the object.
 
 
 
 5. What is object destructuring?
 
-  Your answer:
+  Your answer: Object destructuring is a unique approach to assigning a variable in Javascript. You always will start with a keyword variable when object destructing. When you use destructuring you need to list the names of the variables in parentheses.
 
-  Researched answer:
+  Researched answer: After researching object destructuring, I was wrong on my answer that you need to list the names of the variables in parentheses. You will actually list the names using curly braces. Using object destructuring can save the user a lot of time with typing when the code has objects with properties that are nested. The user can extract more than one property from the array at a time.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I do not remember the term housing form the previous lectures. If I had to make a guess it would be moving a variable to the top of the code 
 
-  Researched answer:
+  Researched answer: After researching I found that I was partially correct with moving a variable up. But there is more to that then just moving up. Hoisting in Javascript allow the function to be used in a safe manner before the being declared. It's not just variables that are moved to the top of their scope but classes are as well. No matter where the variable or function is declared, they are moved to the top no matter if they are global or local.
 
 
 
@@ -58,12 +58,12 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance: Allow the user to make and define a class that takes the functional portion from the parent class and allows the user to add more to it in the code.
 
-2. React:
+2. React: Is a library in JavaScript for the purpose of creating user interfaces. Every single thing in React is comprised of a component. These components are pieces of code that are reusable and are also independent. React is used to create web pages for desktop and mobile ones as well. React can also build applications for smartphones.
 
-3. React state:
+3. React state: is comprised of an object that stores the data of the component. Its goal is to determine how the component will behave and render. The value of the state must be allowed to be changed frequently by the application which makes the component a dynamic one.
 
-4. React lifecycle methods:
+4. React lifecycle methods: breaks down into three methods which are mounting, updating, and unmounting. Mounting is when a component is put into the DOM. Updating is when a piece in the component is updated which can be the state object. Lastly, unmounting is the component that was put into the DOM is removed.
 
-5. DOM:
+5. DOM: DOM stands for document object model which is a programming interface that a is used for web documents. It allows the page to change the document style, structure, and content. The DOM also represents the document as objects and nodes allowing programing languages to interact with the webpage.
